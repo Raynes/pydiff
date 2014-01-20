d9a7dacd17239fcafc5653e5b9829eceb85811be
Anthony Grimes <i@raynes.me>
2014-01-19 23:04:13 -0800

    Add some meta files.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pydiff',
+    version='1.0',
+    long_description=__doc__,
+    packages=['pydiff'],
+    include_package_data=True,
+    zip_safe=False
+)
