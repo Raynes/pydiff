8cc5dec8b3eb059f64e9601645b3bac39227840f
Anthony Grimes <i@raynes.me>
2014-01-19 23:30:47 -0800

    Add some configuration.

--- runserver.py
+++ runserver.py
@@ -1,4 +1,7 @@
+import os
 from pydiff import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT') or 5000)
+    debug = os.environ.get('DEBUG')
+    app.run(debug=debug, port=port)
